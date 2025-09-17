0270b4f927a9e96815f0f43c728131194e398248
Pietro Zardini <pizardini@gmail.com>
2025-09-17 18:07:20 -0300

    adição de tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import VotacoesList from "./components/VotacoesList";
 
 function App() {
   return (
-    <div style={{ fontFamily: "sans-serif", padding: "2rem" }}>
-      <h1>Olho no Voto</h1>
-      <VotacoesList />
+    <div className="p-4 sm:p-8 md:p-12 font-sans">
+      <h1 className="text-lg sm:text-xl md:text-2xl text-center">Olho no Voto</h1>
+        <div className="mt-4 text-sm sm:text-base md:text-lg">
+        <VotacoesList />
+        </div>
+      
+      
     </div>
   );
 }
